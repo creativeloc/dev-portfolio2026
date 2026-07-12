d03a25e836922d72ec23c30577ec78f52e62f650
creativeloc <creativeloc@yahoo.com>
2026-07-12 12:01:33 -0400

    implemented routing varities and light/dark theme toggler function

--- app/page.tsx
+++ app/page.tsx
@@ -4,3 +4,8 @@ const HomePage = () => {
   return <Home />
 }
 export default HomePage
+
+// Home Page:               localhost:3000
+// Others TopLevel Pages:   localhost:3000/about
+// Nested Routes:           localhost:3000/about/team
+// Dynamic Pages:           localhost:3000/about/team/team-member's name
