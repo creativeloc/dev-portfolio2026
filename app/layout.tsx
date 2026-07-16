da4c4bdb9108a6f817a9a829ec1524d3a1147d74
Lawrence Caulker <locaulker@gmail.com>
2026-07-16 10:30:32 -0400

    completed footter section

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Provider from "@/components/Hoc/Provider"
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav"
+import Footer from "@/components/Home/Footer/Footer"
 
 const font = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -29,6 +30,7 @@ export default function RootLayout({
         <Provider>
           <ResponsiveNav />
           {children}
+          <Footer />
         </Provider>
       </body>
     </html>
